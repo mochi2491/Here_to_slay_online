6ca4ca5174fea38e547cedfbe7afef02c4e4670a
havely2491 <westerngeorge2491@gmail.com>
2022-12-24 14:32:39 +0900

    iroirio

--- HeretoSlayOnline/Assets/Scripts/GameCore.cs
+++ HeretoSlayOnline/Assets/Scripts/GameCore.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
+using UniRx;
 
 public class GameCore : MonoBehaviour
 {
+    public ServerConnector connector;
     void Start()
     {
-        
+        connector._receivedMessage.Subscribe(
+            x => {
+                ApplyRecentBoard(x);
+                }
+        );
     }
 
     // Update is called once per frame
@@ -14,9 +21,47 @@ public class GameCore : MonoBehaviour
     {
         
     }
+
+    private void ApplyRecentBoard(string boardText) {
+        
+    }
 }
 public class GameBoardModel : MonoBehaviour {
 
+}
+
+public class ServerConnector : MonoBehaviour {
+    private WebSocket ws;
+    private ReactiveProperty<string> receivedMessage;
+    public IReactiveProperty<string> _receivedMessage => receivedMessage;
+    void Start() {
+        ws = new WebSocket("wss://htsserver.5m8d.net/");
+        ws.OnOpen += (sender, e) => {
+            Debug.Log("Connect to server.");
+        };
+        ws.OnMessage += (sender, e) => {
+            Debug.Log("received message.");
+            receivedMessage.Value = e.Data;
+        };
+
+        ws.OnError += (sender, e) => {
+            Debug.Log("WebSocket Error Message: " + e.Message);
+        };
+
+        ws.OnClose += (sender, e) => {
+            Debug.Log("disconnect to server.");
+        };
+        ws.Connect();
+    }
+
+    void Update() {
+    }
+
+
+    void OnDestroy() {
+        ws.Close();
+        ws = null;
+    }
 }
 
 struct GameBoardData {
