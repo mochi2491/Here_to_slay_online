184b6daed0a555140febab206dac98ad4ff748f1
monchi3 <55975000+mochi2491@users.noreply.github.com>
2023-01-23 15:47:10 +0900

    Update CardPresenter.cs

--- HeretoSlayOnline/Assets/Scripts/CardPresenter.cs
+++ HeretoSlayOnline/Assets/Scripts/CardPresenter.cs
@@ -9,28 +9,15 @@ public class CardPresenter : MonoBehaviour
     public GameCore gameCore;
 
     //view
-    //手札表示
-    public GameObject handObject;
-    //各プレイヤーのヒーロー
-    public List<GameObject> heroObject = new List<GameObject>();
-    //倒したモンスター
-    public List<GameObject> slayedmonsterObject = new List<GameObject>();
-    //捨て札
-    public GameObject discardpileObject;
-    //モンスター
-    public GameObject monsterObject;
-
-    public List<CardView> Hand;
-    public List<CardView> Discard;
-    public List<List<CardView>> Hero;
-    public List<CardView> monsterList;
-    public List<CardView> slayerMonser;
+    public GameBoardView gameBoardView;
 
     private void Start() {
         gameCore._gameBoard.Subscribe(
             board => {
+                //initial
+                //arrange cards
 
-
+                //subscribe
             }
             );
     }
